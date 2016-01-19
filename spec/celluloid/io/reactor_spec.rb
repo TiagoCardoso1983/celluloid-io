4050ce0fc4e88c55179f393d33d7820e5f70ad1d
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-01-19 15:03:15 +0100

    reactor spec no longer leaking to the rest

--- spec/celluloid/io/reactor_spec.rb
+++ spec/celluloid/io/reactor_spec.rb
@@ -5,35 +5,35 @@ RSpec.describe Celluloid::IO::Reactor, library: :IO do
   let(:example_port) { assign_port }
 
   it "shouldn't crash" do
-    server = ::TCPServer.new example_addr, example_port
+    begin
+      server = ::TCPServer.new example_addr, example_port
+      thread = Thread.new { server.accept }
+      socket = within_io_actor { Celluloid::IO::TCPSocket.new example_addr, example_port }
+      peer = thread.value
+      peer_thread = Thread.new { loop { peer << payload } }
+      handle = false
 
-    thread = Thread.new { server.accept }
-
-    socket = within_io_actor { Celluloid::IO::TCPSocket.new example_addr, example_port }
-    peer = thread.value
-    peer_thread = Thread.new { loop { peer << payload } }
-    handle = false
-
-    # Main server body:
-    within_io_actor do
-      begin
-        timeout(2) do
-          loop do
-            socket.readpartial(2046)
+      # Main server body:
+      within_io_actor do
+        begin
+          timeout(2) do
+            loop do
+              socket.readpartial(2046)
+            end
           end
+        # rescuing timeout, ok. rescuing terminated exception, is it ok? TODO: investigate
+        rescue Celluloid::TaskTerminated, Celluloid::TaskTimeout, Timeout::Error
+        ensure
+          socket.readpartial(2046)
+          handle = true
         end
-      # rescuing timeout, ok. rescuing terminated exception, is it ok? TODO: investigate
-      rescue Celluloid::TaskTerminated, Celluloid::TaskTimeout, Timeout::Error
-      ensure
-        socket.readpartial(2046)
-        handle = true
       end
+
+      expect(handle).to be_truthy
+    ensure
+      server.close
+      peer.close
+      socket.close
     end
-
-    expect(handle).to be_truthy
-
-    server.close
-    peer.close
-    socket.close
   end
 end
